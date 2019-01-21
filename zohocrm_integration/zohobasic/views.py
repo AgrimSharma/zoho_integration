5729512c8d606877a9a6782b11be310ec0de7a5f
agrim <agrim89@gmail.com>
2019-01-21 18:36:24 +0530

    updated 20190121 1659

--- zohocrm_integration/zohobasic/views.py
+++ zohocrm_integration/zohobasic/views.py
@@ -95,6 +95,7 @@ def callback(request):
         tasks = all_projects_task()
         milestone = all_projects_milestone()
         timesheet= all_project_time_sheet()
+        projects = Projects.objects.all()
         # return HttpResponse(json.dumps(dict(projects=projects, tokens=vals)))
         return render(request, "main.html", {"projects":projects})
         # return HttpResponse(json.dumps(dict(message="Success")))
