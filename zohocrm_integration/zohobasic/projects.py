5d59c20e50502daaeaeb0a4c3863225bb3a3f630
agrim <agrim89@gmail.com>
2019-01-31 12:36:48 +0530

    iupdated 20190131 1232

--- zohocrm_integration/zohobasic/projects.py
+++ zohocrm_integration/zohobasic/projects.py
@@ -135,25 +135,24 @@ def project_list_view(name):
                 color = 'green'
         except Exception:
             pass
-        else:
-            data = dict(name=pro.name,
-                        id=pro.id,
-                        end_date=pro.end_date_format,
-                        task_count_open=pro.task_count_open + pro.task_count_close,
-                        milestone_count_open=pro.milestone_count_open + pro.milestone_count_close,
-                        task_count_close=pro.task_count_close,
-                        milestone_count_close=pro.milestone_count_close,
-                        start_date=pro.start_date_format,
-                        status=pro.status.capitalize(),
-                        created_date=pro.created_date_format,
-                        project_id=pro.project_id,
-                        current_task=len(current_task),
-                        future_date_one_week=len(future_date_one_week),
-                        past_date_one_week=len(past_date_one_week),
-                        past_date_two_week=len(past_date_two_week),
-                        percent=percent,
-                        color=color,
-                        csm=pro.owner_name
-                        )
-            response.append(data)
+        data = dict(name=pro.name,
+                    id=pro.id,
+                    end_date=pro.end_date_format,
+                    task_count_open=pro.task_count_open + pro.task_count_close,
+                    milestone_count_open=pro.milestone_count_open + pro.milestone_count_close,
+                    task_count_close=pro.task_count_close,
+                    milestone_count_close=pro.milestone_count_close,
+                    start_date=pro.start_date_format,
+                    status=pro.status.capitalize(),
+                    created_date=pro.created_date_format,
+                    project_id=pro.project_id,
+                    current_task=len(current_task),
+                    future_date_one_week=len(future_date_one_week),
+                    past_date_one_week=len(past_date_one_week),
+                    past_date_two_week=len(past_date_two_week),
+                    percent=percent,
+                    color=color,
+                    csm=pro.owner_name
+                    )
+        response.append(data)
     return response
